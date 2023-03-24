ac057e6266e91498fe7c3935f095113a88074e3b
root <root@ip-172-31-27-109.ca-central-1.compute.internal>
2023-03-24 11:59:12 +0000

    siva

---
+++ siva.sh
@@ -0,0 +1,15 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+amazon-linux-extras install java-openjdk11 -y
+yum install jenkins -y
+yum install git -y
+yum install docker -y
+systemctl start jenkins
+systemctl status jenkins
+
+
+
